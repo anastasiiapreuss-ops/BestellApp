bb400d22de45269fd08fea669af18c7806f68cfb
Anastasiia Preuß <anastasiiapreuss@gmail.com>
2026-06-24 14:28:09 +0200

    add: rest css, js

--- script.js
+++ script.js
@@ -1,154 +1,164 @@
 const dishes = [
-  {
-    "name": "Coq au Vin",
-    "image": "./assets/img/coq-au-vin.jpg",
-    "price": 31.99,
-    "ingredients": ["Huhn", "Speck", "Champignons", "Kräuter"]
-  },
-  {
-    "name": "Boeuf Bourguignon",
-    "image": "./assets/img/boeuf-bourguignon.jpg",
-    "price": 35.99,
-    "ingredients": ["Rindfleisch", "Burgunderwein", "Schalotten", "Karotten"]
-  },
-  {
-    "name": "Soupe à l’Oignon",
-    "image": "./assets/img/soupe-à-l’oignon.jpg",
-    "price": 22.99,
-    "ingredients": ["Zwiebeln", "Rinderbrühe", "Baguette", "Käse"]
-  },
-  {
-    "name": "Quiche Lorraine",
-    "image": "./assets/img/quiche-lorraine.jpg",
-    "price": 25.99,
-    "ingredients": ["Mürbeteig", "Speck", "Eier", "Sahne", "Käse"]
-  },
-  {
-    "name": "Ratatouille",
-    "image": "./assets/img/ratatouille.jpg",
-    "price": 21.99,
-    "ingredients": ["Auberginen", "Zucchini", "Paprika", "Tomaten", "Knoblauch"]
-  },
-  {
-    "name": "Crème Brûlée",
-    "image": "./assets/img/crème_brûlée.jpg",
-    "price": 10.99,
-    "ingredients": ["Sahne", "Eigelb", "Vanille", "brauner Zucker"]
-  }
+    {
+        "name": "Coq au Vin",
+        "image": "./assets/img/coq-au-vin.jpg",
+        "price": 31.99,
+        "ingredients": ["Huhn", " Speck", " Champignons", " Kräuter"]
+    },
+    {
+        "name": "Boeuf Bourguignon",
+        "image": "./assets/img/boeuf-bourguignon.jpg",
+        "price": 35.99,
+        "ingredients": ["Rindfleisch", " Burgunderwein", " Schalotten", " Karotten"]
+    },
+    {
+        "name": "Soupe à l’Oignon",
+        "image": "./assets/img/soupe-à-l’oignon.jpg",
+        "price": 22.99,
+        "ingredients": ["Zwiebeln", " Rinderbrühe", " Baguette", " Käse"]
+    },
+    {
+        "name": "Quiche Lorraine",
+        "image": "./assets/img/quiche-lorraine.jpg",
+        "price": 25.99,
+        "ingredients": ["Mürbeteig", " Speck", " Eier", " Sahne", " Käse"]
+    },
+    {
+        "name": "Ratatouille",
+        "image": "./assets/img/ratatouille.jpg",
+        "price": 21.99,
+        "ingredients": ["Auberginen", " Zucchini", " Paprika", " Tomaten", " Knoblauch"]
+    },
+    {
+        "name": "Crème Brûlée",
+        "image": "./assets/img/crème_brûlée.jpg",
+        "price": 10.99,
+        "ingredients": ["Sahne", " Eigelb", " Vanille", " brauner Zucker"]
+    }
 ];
-
-
 
 let shoppingCart = {};
 let subtotal = 0;
 let total = 0;
 
 function init() {
-  renderDishes();
-  renderCart();
+    renderDishes();
+    renderCart();
 }
 
 function renderDishes() {
-  const renderDishesRef = document.getElementById('menu');
-  renderDishesRef.innerHTML = "";
-  for (let i = 0; i < dishes.length; i++) {
-    renderDishesRef.innerHTML += getTemplateDishes(i)
-  }
+    const renderDishesRef = document.getElementById('menu');
+    renderDishesRef.innerHTML = "";
+    for (let i = 0; i < dishes.length; i++) {
+        renderDishesRef.innerHTML += getTemplateDishes(i)
+    }
 }
 
-
-
 function addDishes(i) {
-  let addButtonRef;
-  if (!([i] in shoppingCart)) {
-    shoppingCart[i] = 1;
-    addButtonRef = document.getElementById(`active-button${i}`).innerHTML = `Hinzugefügt`;
-    addButtonRef = document.getElementById(`active-button${i}`).classList.add("added_button_style");
-  } else {
-    shoppingCart[i] += 1;
-    addButtonRef = document.getElementById(`active-button${i}`).innerHTML = `Hinzugefügt`;
-    addButtonRef = document.getElementById(`active-button${i}`).classList.add("added_button_style");
-  }
-  calculateSum()
-  renderCart();
+    let addButtonRef;
+    if (!([i] in shoppingCart)) {
+        shoppingCart[i] = 1;
+        addButtonRef = document.getElementById(`active-button${i}`).innerHTML = `Hinzugefügt`;
+        addButtonRef = document.getElementById(`active-button${i}`).classList.add("added_button_style");
+    } else {
+        shoppingCart[i] += 1;
+        addButtonRef = document.getElementById(`active-button${i}`).innerHTML = `Hinzugefügt`;
+        addButtonRef = document.getElementById(`active-button${i}`).classList.add("added_button_style");
+    }
+    calculateSum()
+    renderCart();
 }
 
 function renderCart() {
-  subtotal = 0;
-  total = 0;
-  const renderCartRef = document.getElementById('shopping-cart-content')
-  renderCartRef.innerHTML = "";
-  let keys = Object.keys(shoppingCart);
-  for (let j = 0; j < keys.length; j++) {
-    let key = keys[j];
-    renderCartRef.innerHTML += getCartTemplate(key);
-  }
+    subtotal = 0;
+    total = 0;
+    const renderCartRef = document.getElementById('shopping-cart-content')
+    renderCartRef.innerHTML = "";
+    let keys = Object.keys(shoppingCart);
+    if (keys.length === 0) {
+        renderCartRef.innerHTML = getEmptyCartTemplate();
+        return;
+    }
+    for (let j = 0; j < keys.length; j++) {
+        let key = keys[j];
+        renderCartRef.innerHTML += getCartTemplate(key);
+    }
 }
 
 function calculateSum() {
-  let amount = 0;
-  let keys = Object.keys(shoppingCart);
-  for (let j = 0; j < keys.length; j++) {
-    let key = keys[j];
-    subtotal += shoppingCart[key] * dishes[key].price;
-    total = 10.99 + subtotal;
-    amount += shoppingCart[key];
-  }
-  document.getElementById('sub-total-price').innerHTML = subtotal.toFixed(2).replace(".", ",") + " €";
-  document.getElementById('total-price').innerHTML = total.toFixed(2).replace(".", ",") + " €";
-  document.getElementById('total-price-button').innerHTML = "Jetzt bezahlen (" + total.toFixed(2).replace(".", ",") + "€)";
-  document.getElementById('cart-amount').innerHTML = amount;
+    let amount = 0;
+    let keys = Object.keys(shoppingCart);
+    for (let j = 0; j < keys.length; j++) {
+        let key = keys[j];
+        subtotal += shoppingCart[key] * dishes[key].price;
+        total = 10.99 + subtotal;
+        amount += shoppingCart[key];
+    }
+    document.getElementById('sub-total-price').innerHTML = subtotal.toFixed(2).replace(".", ",") + " €";
+    document.getElementById('total-price').innerHTML = total.toFixed(2).replace(".", ",") + " €";
+    document.getElementById('total-price-button').innerHTML = "Jetzt bezahlen (" + total.toFixed(2).replace(".", ",") + "€)";
+    document.getElementById('cart-amount').innerHTML = amount;
+    updateCartButton(amount);
 }
 
 function changeAmount(key, value) {
-  let addButtonRef = document.getElementById(`active-button${key}`)
-  shoppingCart[key] += value;
-  if (shoppingCart[key] <= 0) {
-    delete shoppingCart[key];
-    addButtonRef.innerHTML = "Hinzufügen";
-    addButtonRef.classList.remove("added_button_style");
-  }
-  calculateSum();
-  renderCart();
+    let addButtonRef = document.getElementById(`active-button${key}`)
+    shoppingCart[key] += value;
+    if (shoppingCart[key] <= 0) {
+        delete shoppingCart[key];
+        addButtonRef.innerHTML = "Hinzufügen";
+        addButtonRef.classList.remove("added_button_style");
+    }
+    calculateSum();
+    renderCart();
 }
 
 function openDialoge() {
-  const dialogRef = document.getElementById('dialog-input');
-  dialogRef.innerHTML = getDialogTemplate();
-  dialogRef.showModal();
+    const dialogRef = document.getElementById('dialog-input');
+    dialogRef.innerHTML = getDialogTemplate();
+    dialogRef.showModal();
 }
 
 function buyNow() {
-  const buyNowRef =document.getElementById('shopping-cart')
+    const buyNowRef = document.getElementById('shopping-cart')
     buyNowRef.classList.add('shopping_cart_style_hidden');
     buyNowRef.classList.remove("shopping_cart_mobile");
-
-  openDialoge();
+    openDialoge();
 }
 
 function closeAllDialoge() {
-  const dialogRef = document.getElementById('dialog-input');
-  const cart = document.getElementById('shopping-cart');
-  cart.classList.remove("shopping_cart_mobile");
-
-  shoppingCart = {};
-  renderCart();
-  calculateSum();
-  resetAddButtons();
-  dialogRef.close();
+    const dialogRef = document.getElementById('dialog-input');
+    const cart = document.getElementById('shopping-cart');
+    cart.classList.remove("shopping_cart_mobile");
+    shoppingCart = {};
+    renderCart();
+    calculateSum();
+    resetAddButtons();
+    dialogRef.close();
 }
 
 function resetAddButtons() {
-
-  for (let i = 0; i < dishes.length; i++) {
-    let resetButtonRef = document.getElementById(`active-button${i}`);
-    resetButtonRef.innerHTML = "Hinzufügen";
-    resetButtonRef.classList.remove("added_button_style");
-
-  }
+    for (let i = 0; i < dishes.length; i++) {
+        let resetButtonRef = document.getElementById(`active-button${i}`);
+        resetButtonRef.innerHTML = "Hinzufügen";
+        resetButtonRef.classList.remove("added_button_style");
+    }
 }
 
 function mobileShoppingCart() {
-  const cartRef = document.getElementById('shopping-cart');
-  cartRef.classList.toggle('shopping_cart_mobile');
+    const cartRef = document.getElementById('shopping-cart');
+    cartRef.classList.toggle('shopping_cart_mobile');
+}
+
+function updateCartButton(amount) {
+    const cartButtonRef = document.getElementById('cart-button');
+    const cartAmountRef = document.getElementById('cart-amount')
+    if (amount > 0) {
+        cartButtonRef.classList.add("cart_button_active");
+        cartAmountRef.classList.add("cart_amount_active");
+    } else {
+        cartButtonRef.classList.remove("cart_button_active");
+        cartAmountRef.classList.remove("cart_amount_active");
+    }
+
 }
--- tempalte.js
+++ tempalte.js
@@ -1,21 +1,21 @@
-function getTemplateDishes (i){
+function getTemplateDishes(i) {
     return ` 
       <section class="dishes_style">
                 <img class="dishes_img_style" src= "${dishes[i].image}" alt="${dishes[i].name}">
-                    <section class = "dishes_text_style">
+                <section class = "dishes_text_style">
                     <div class = "name_dishes_style">
                         <h3>${dishes[i].name}</h3>
-                            <p>${dishes[i].ingredients}</p>
-                        </div>
-                        <div class = "top_text_content">
-                            <p>${dishes[i].price.toFixed(2).replace(".", ",")} €</p>
-                            <button id ="active-button${i}" onclick = "addDishes(${i})" class = "add_button_style">Hinzufügen</button>
-                        </div>
-                    </section>
+                        <p>${dishes[i].ingredients}</p>
+                    </div>
+                    <div class = "top_text_content">
+                        <p>${dishes[i].price.toFixed(2).replace(".", ",")} €</p>
+                        <button id ="active-button${i}" onclick = "addDishes(${i})" class = "add_button_style">Hinzufügen</button>
+                    </div>
+                </section>
         </section>`;
 }
-   
-function getCartTemplate (key){
+
+function getCartTemplate(key) {
     return `
    <section class="cart_style">
         <p>${shoppingCart[key]} x ${dishes[key].name}</p>
@@ -25,20 +25,29 @@ function getCartTemplate (key){
                     ${shoppingCart[key]}
                 <button onclick="changeAmount(${key}, +1)">+</button>
             </section> 
-                <p>${(shoppingCart[key] * dishes[key].price).toFixed(2).replace(".", ",")}€</p>
+            <p>${(shoppingCart[key] * dishes[key].price).toFixed(2).replace(".", ",")}€</p>
         </section>
     </section>
     `
-   
 }
 
-function getDialogTemplate(){
-    return`
-    <section class = "dialog_style">
-    <button class ="x_button" onclick ="closeAllDialoge()">x</button>
-      <img src="./assets/icons/food-truck-icon.png" alt="Food Truck icon">
+function getDialogTemplate() {
+    return `
+    <section class="dialog_style">
+        <button class="x_button" onclick="closeAllDialoge()">x</button>
+        <img src="./assets/icons/food-truck-icon.png" alt="Food Truck icon">
         <h3>Bestellung bestätigt!</h3>
         <p>Ihr Essen ist auf dem Weg</p>
     </section>
     `
+}
+
+function getEmptyCartTemplate(){
+     return `
+        <section class="empty_cart_style">
+            <h3>Ihr Warenkorb ist leer</h3>
+            <p>Fügen Sie leckere Gerichte hinzu und starten Sie die Bestellung.</p>
+            <img src="./assets/icons/basket.svg" alt="warenkorb icon">
+        </section>
+    `;
 }
